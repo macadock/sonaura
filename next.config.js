8050c5efc07e4f8abf6f72778d291f6bdfd34d89
Mathieu Barrière <macadock@users.noreply.github.com>
2023-05-02 17:01:17 +0200

    chore(next.config.js): add modularize imports for @mui

--- next.config.js
+++ next.config.js
@@ -21,6 +21,20 @@ const moduleExports = {
       },
     ];
   },
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+    },
+    '@mui/x-data-grid': {
+      transform: '@mui/x-data-grid/{{member}}',
+    },
+    '@mui/x-date-pickers': {
+      transform: '@mui/x-date-pickers/{{member}}',
+    },
+  },
 };
 
 const sentryWebpackPluginOptions = {
