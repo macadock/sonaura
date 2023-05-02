2afc1295c4b5bea953f450129df187c10cd83429
Mathieu Barrière <tech@barriere.me>
2023-05-02 16:30:28 +0200

    fix(product-edit): fix app crash

--- pages/dashboard/products/[id].tsx
+++ pages/dashboard/products/[id].tsx
@@ -1,30 +1,23 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import i18nConfig from 'next-i18next.config';
 import DashboardEditProductView from 'views/Dashboard/Products/DashboardEditProductView';
-import { getProducts } from 'lib/supabase/products';
 
 const DashboardEditProduct: NextPage = () => {
   return <DashboardEditProductView />;
 };
 
-export async function getStaticPaths() {
-  const { data } = await getProducts();
-  return {
-    paths: data.map((product) => ({
-      params: {
-        id: product.id,
-      },
-    })),
-    fallback: true,
-  };
-}
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
+  const translations = await serverSideTranslations(
+    locale,
+    ['common'],
+    i18nConfig,
+  );
 
-export const getStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale, ['common'], i18nConfig)),
+      ...translations,
     },
   };
 };
