e2ee8456f4371650a08b49fecb28e4da29dc4f15
Mathieu Barrière <tech@barriere.me>
2023-07-27 17:07:34 +0200

    feat(dashboard): add realisations to dashboard

--- components/core/Installations/Main/index.tsx
+++ components/core/Installations/Main/index.tsx
@@ -1,41 +1,22 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Column from './Column';
 import { Installation } from 'lib/supabase/installations';
+import Card from 'components/core/Installations/Main/Card';
 
 interface Props {
   installations: Installation[];
 }
 
 const Main: React.FC<Props> = ({ installations }) => {
-  const instArray: [Installation[], Installation[], Installation[]] = [
-    [],
-    [],
-    [],
-  ];
-
-  let counter = 0;
-
-  installations.map((installation) => {
-    counter > 2 ? (counter = 0) : null;
-    instArray[counter] = [...instArray[counter], installation];
-
-    return counter++;
-  });
-
   return (
     <Box sx={{ marginBottom: '2rem', marginTop: '2rem' }}>
       <Grid container spacing={4}>
-        <Grid item xs={12} md={4}>
-          <Column installations={instArray[0]} />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Column installations={instArray[1]} />
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Column installations={instArray[2]} />
-        </Grid>
+        {installations.map((installation) => (
+          <Grid key={installation.id} item xs={12} sm={6} lg={4}>
+            <Card installation={installation} />
+          </Grid>
+        ))}
       </Grid>
     </Box>
   );
