ec7dab407d00253b402a5987d4f06b81a065b57b
Mathieu Barriere <tech@barriere.me>
2024-06-10 14:26:09 +0200

    chore(auth): add auth by magic link

--- utils/auth/index.ts
+++ utils/auth/index.ts
@@ -1,4 +1,3 @@
 export { redirectUserToPage } from './redirect-homepage';
 export { handleSignIn } from './sign-in';
 export { handleSignOut } from './sign-out';
-export { handleSignUp } from './sign-up';
