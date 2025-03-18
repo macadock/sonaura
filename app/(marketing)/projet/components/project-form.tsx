c41bbff3d67251bf151906c88d17161fc41b9659
Mathieu Barriere <tech@barriere.me>
2025-03-18 15:19:55 +0100

    Add customer email submission and success feedback
    Integrated email submission logic using SendInBlue API and updated the form with confirmation feedback upon successful submission. Disabled the submit button while processing to enhance user experience.

--- app/(marketing)/projet/components/project-form.tsx
+++ app/(marketing)/projet/components/project-form.tsx
@@ -16,6 +16,9 @@ import {
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
+import { ApiUrls, getRoutePath } from '@/appConstants';
+import SendEmailInput from '@/SendInBlue/dto/send-customer-email.input';
+import { CheckCircle } from 'lucide-react';
 
 const formSchema = z.object({
   firstName: z.string().min(2, {
@@ -48,8 +51,39 @@ export const ProjectForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  const isSubmitting = form.formState.isSubmitting;
+  const isSubmitted = form.formState.isSubmitted;
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const { firstName, lastName, email, phoneNumber, message, shop } = values;
+
+    const messageBody = `
+    Boutique sélectionnée: ${shop} \n
+    ${message}
+    `;
+
+    const customerBody: SendEmailInput = {
+      firstName,
+      lastName,
+      email,
+      phone: phoneNumber,
+      message: messageBody,
+      templateId: {
+        customer: 3,
+        merchant: 4,
+      },
+    };
+
+    const url = getRoutePath({ api: ApiUrls.SEND_CUSTOMER_EMAIL });
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(customerBody),
+    });
+
+    if (response.ok) {
+      form.reset();
+    }
   }
 
   return (
@@ -153,10 +187,16 @@ export const ProjectForm = () => {
             </FormItem>
           )}
         />
-        <div className={'flex'}>
-          <Button size={'lg'} type="submit">
+        <div className={'flex gap-2'}>
+          <Button size={'lg'} type="submit" disabled={isSubmitting}>
             Envoyer
           </Button>
+          {isSubmitted && (
+            <div className={'flex items-center gap-2 md:col-span-2'}>
+              <CheckCircle className={'size-4'} />
+              <p>Demande envoyée avec succès.</p>
+            </div>
+          )}
         </div>
       </form>
     </Form>
